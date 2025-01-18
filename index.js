41c1b6a0b3eecf355e1f33cf692d6804fc543e9a
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-01-18 12:51:59 -0700

    second commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let firstVariable=prompt("what is 3 + 7?")
+let secondVariable=prompt("what is 50 - 10?")
+let thirdVariable=prompt("what is 3 * 13?")
+alert(`${firstVariable} - ${secondVariable} - ${thirdVariable}`)
